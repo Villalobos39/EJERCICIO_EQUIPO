6206da744e086f05fda2b5c13862a2e13d54eba7
alondra-luna <lunalondra27@gmail.com>
2022-04-26 17:51:56 -0500

    prueba 22
    prueba 22

--- CUENTA_BANCARIA/CalcularCuenta.cs
+++ CUENTA_BANCARIA/CalcularCuenta.cs
@@ -9,6 +9,7 @@ namespace CUENTA_BANCARIA
     class CalcularCuenta
     {
         SaldoFinal SaldoFinal = new SaldoFinal();
+        Form1 Form1 = new Form1();
         public decimal CuentaAhorroMensual(double Interes, string Periodo, decimal DepositoInicial, decimal DepositoAhorro)
         {
             int Mensualidades;
@@ -19,23 +20,23 @@ namespace CUENTA_BANCARIA
             {
                 case "1 AÑO":
                     Mensualidades = Meses;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Mensualidades, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Mensualidades, Interes);
                     break;
                 case "2 AÑOS":
                     Mensualidades = Meses * 2;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Mensualidades, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Mensualidades, Interes);
                     break;
                 case "3 AÑOS":
                     Mensualidades = Meses * 3;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Mensualidades, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Mensualidades, Interes);
                     break;
                 case "4 AÑOS":
                     Mensualidades = Meses * 4;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Mensualidades, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Mensualidades, Interes);
                     break;
                 case "5 AÑOS":
                     Mensualidades = Meses * 5;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Mensualidades, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Mensualidades, Interes);
                     break;
             }
             return Total;
@@ -49,23 +50,23 @@ namespace CUENTA_BANCARIA
             {
                 case "1 AÑO":
                     Semestres = 2;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Semestres, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Semestres, Interes);
                     break;
                 case "2 AÑOS":
                     Semestres = 4;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Semestres, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Semestres, Interes);
                     break;
                 case "3 AÑOS":
                     Semestres = 6;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Semestres, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Semestres, Interes);
                     break;
                 case "4 AÑOS":
                     Semestres = 8;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Semestres, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Semestres, Interes);
                     break;
                 case "5 AÑOS":
                     Semestres=10;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Semestres, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Semestres, Interes);
                     break;
             }
             return Total;
@@ -79,23 +80,23 @@ namespace CUENTA_BANCARIA
             {
                 case "1 AÑO":
                     Anual = 1;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Anual, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Anual, Interes);
                     break;
                 case "2 AÑOS":
                     Anual = 2;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Anual, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Anual, Interes);
                     break;
                 case "3 AÑOS":
                     Anual = 3;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Anual, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Anual, Interes);
                     break;
                 case "4 AÑOS":
                     Anual = 4;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Anual, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Anual, Interes);
                     break;
                 case "5 AÑOS":
                     Anual = 5;
-                    Total = SaldoFinal.SaldoTotal(DepositoInicial, DepositoAhorro, Anual, Interes);
+                    Total = Form1.SaldoTotal(DepositoInicial, DepositoAhorro, Anual, Interes);
                     break;
             }
             return Total;
